6990221c8c75d10b07c247fb9c8e6c3ef9b93980
Malik <malik@hey.com>
2024-04-29 15:16:35 +0200

    Added on_hover animation to Malik's motto.

--- components/motto.tsx
+++ components/motto.tsx
@@ -5,7 +5,7 @@
  */
 export function Motto() {
   return (
-    <p className='uppercase text-xs [writing-mode:vertical-lr] rotate-180 fixed bottom-20 tracking-wider mx-3 hover:text-tertiary text-slate-400 max-sm:hidden transition-all duration-300 cursor-default'>
+    <p className='uppercase text-xs [writing-mode:vertical-lr] rotate-180 fixed bottom-20 tracking-wider mx-3 hover:animate-pulse text-slate-400 max-sm:hidden transition-all duration-300 cursor-default'>
       Only you know who you can be
     </p>
   );
