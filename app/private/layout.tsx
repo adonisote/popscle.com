be007ba6ce7bcb1330bc5077d299091de5f43745
adonisote <adonis.almagro@code.berlin>
2024-06-12 15:54:59 +0200

    Resources redesign

--- app/private/layout.tsx
+++ app/private/layout.tsx
@@ -1,12 +1,24 @@
-export default function Layout({
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
+export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const supabase = createClient()
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    redirect('/login')
+  }
+
   return (
     <html>
       <body>
-        <main className="min-h-screen flex flex-col items-center">
+        <main className="h-screen w-screen flex flex-col items-center justify-center">
           {children}
         </main>
       </body>
