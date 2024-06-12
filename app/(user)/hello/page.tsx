a69ca0c71ba1ee340a186380fc984dde7743a937
adonisote <adonis.almagro@code.berlin>
2024-06-12 11:16:48 +0200

    link to profile from hello page

--- app/(user)/hello/page.tsx
+++ app/(user)/hello/page.tsx
@@ -1,6 +1,8 @@
 import { createClient } from "@/utils/supabase/server"
 import { redirect } from "next/navigation"
 import { Button } from "@/components/ui/button"
+import Link from "next/link"
+
 
 export default async function Page() {
   const supabase = createClient()
@@ -11,6 +13,8 @@ export default async function Page() {
   return (
     <div className=" w-full h-full flex flex-col items-center">
       <div className="w-full flex justify-end p-8">
+        <Link className=" p-2 hover:underline hover:underline-offset-4" href={`/account`}>Profile</Link>
+
         <form action="/auth/signout" method="post">
           <Button type="submit">
             Sign out
