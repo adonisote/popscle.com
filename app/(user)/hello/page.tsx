9c413e27c259e25a95a1e6c92719d5147186d272
adonisote <adonis.almagro@code.berlin>
2024-06-12 10:49:19 +0200

    sign out from hello page

--- app/(user)/hello/page.tsx
+++ app/(user)/hello/page.tsx
@@ -1,5 +1,6 @@
 import { createClient } from "@/utils/supabase/server"
 import { redirect } from "next/navigation"
+import { Button } from "@/components/ui/button"
 
 export default async function Page() {
   const supabase = createClient()
@@ -8,8 +9,20 @@ export default async function Page() {
     redirect('/login')
   }
   return (
+    <div className=" w-full h-full flex flex-col items-center">
+      <div className="w-full flex justify-end p-8">
+        <form action="/auth/signout" method="post">
+          <Button type="submit">
+            Sign out
+          </Button>
+        </form>
+      </div>
+      <div className="w-full h-full flex flex-col items-center justify-center">
+        <p>Hello, {user.email} </p>
 
-    <p>Hello, {user.email} </p>
+      </div>
+
+    </div>
   )
 
 }
