68b0689a0fc049e8af696a016da1625d5d92d01c
adonisote <adonis.almagro@code.berlin>
2024-06-17 14:04:34 +0200

    redirect to /home when trying to access /s

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  //redirect from /s to /home
+  async redirects() {
+    return [
+      {
+        source: '/s',
+        destination: '/home',
+        permanent: true
+      }
+    ]
+  }
+
+};
 
 export default nextConfig;
