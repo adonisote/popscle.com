f9d0f2b8f87da78abd04e4422d6b78b272aa2090
adonisote <adonis.almagro@code.berlin>
2024-06-17 13:40:09 +0200

    dynamic spaces route

---
+++ app/(community)/s/page.tsx
@@ -0,0 +1,37 @@
+import { createClient } from "@/utils/supabase/server";
+import Nav from "@/components/ui/nav";
+import Link from "next/link";
+
+const spaces = ['React', 'NextJs', 'Automotive']
+
+export default async function PrivatePage() {
+  const supabase = createClient()
+  // const { data: { user }, error } = await supabase.auth.getUser()
+  const { data, error } = await supabase
+    .from('resources')
+    .select()
+  console.log(data)
+
+  return (
+    <div className=" w-full h-full flex flex-col items-center">
+      <Nav />
+
+
+      <div className="w-full h-full flex flex-col items-center justify-center">
+        <p className="text-xl">Spaces</p>
+        {/* I need to create a table containing spaces that contain ressources */}
+
+        <div>
+          {spaces.map(space => (
+            <div key={``} className="border rounded-lg m-4 border-slate-200">
+              <Link href={`/s/${space.toLocaleLowerCase()}`} >
+                <p className="text-center p-2">{space}</p>
+              </Link>
+            </div>
+          ))}
+        </div>
+
+      </div>
+    </div>
+  )
+}
