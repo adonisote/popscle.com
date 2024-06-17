029c258fbe5a4ffb9738c1f61fec41ac967c18cb
adonisote <adonis.almagro@code.berlin>
2024-06-18 01:39:35 +0200

    render resources, still need to select only resouces from the specific space

--- app/(community)/s/[slug]/page.tsx
+++ app/(community)/s/[slug]/page.tsx
@@ -1,6 +1,42 @@
-export default function Page({ params }: { params: { slug: string } }) {
+import { createClient } from "@/utils/supabase/server"
+import Link from "next/link"
+
+export default async function Page({ params }: { params: { slug: string } }) {
+  const supabase = createClient()
+
+  const { data: resources, error } = await supabase
+    .from('resources')
+    .select()
+  // need to find a way to select only the resources from the respective space
+  console.log(resources)
+  if (error) {
+    console.error('Error fetching spaces:', error);
+  }
 
   return (
-    <p>Space: {params.slug}</p>
+    <div className="flex flex-col items-center">
+      <p>{params.slug}</p>
+
+      <div>
+        {resources.map(resource => (
+          <div key={resource.id} className="m-4 flex border rounded-lg border-slate-200 hover:bg-primary/90 ">
+            <div className="p-2 flex flex-col items-center justify-center">
+              {resource.votes}
+            </div>
+            <Link
+              href={resource.url}
+            >
+              <p>
+                {resource.title}
+              </p>
+              <p>{resource.description}</p>
+            </Link>
+          </div>
+        ))}
+      </div>
+
+    </div>
+
+
   )
 }
