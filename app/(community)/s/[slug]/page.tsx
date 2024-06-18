a085993c2bd1117ccc6be1f57bb698d74292cee2
adonisote <adonis.almagro@code.berlin>
2024-06-18 13:44:01 +0200

    update Resource Type

--- app/(community)/s/[slug]/page.tsx
+++ app/(community)/s/[slug]/page.tsx
@@ -19,7 +19,7 @@ export default async function Page({ params }: { params: { slug: string } }) {
       <p>{params.slug}</p>
 
       <div>
-        {resources.map((resource: Resource) => (
+        {resources?.map((resource: Resource) => (
           <div key={resource.id} className="m-4 flex border rounded-lg border-slate-200 hover:bg-primary/90 ">
             <div className="p-2 flex flex-col items-center justify-center">
               {resource.votes}
