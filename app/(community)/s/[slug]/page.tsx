bb380d0cf208b5f7262dbdd6615362beb87bf865
adonisote <adonis.almagro@code.berlin>
2024-06-18 18:05:53 +0200

    fetching resources depending on space route

--- app/(community)/s/[slug]/page.tsx
+++ app/(community)/s/[slug]/page.tsx
@@ -2,16 +2,32 @@ import { createClient } from "@/utils/supabase/server"
 import Link from "next/link"
 import { Resource } from "@/types/types"
 
-export default async function Page({ params }: { params: { slug: string } }) {
-  const supabase = createClient()
+export default async function Page({
+  params
+}: {
+  params: { slug: string };
+}) {
+  const supabase = createClient();
 
-  const { data: resources, error } = await supabase
+  const { data: spaces, error: spaceError } = await supabase
+    .from("spaces")
+    .select("id, title")
+    .ilike("title", params.slug)
+  const spaceId = spaces[0].id
+  console.log('spaceId: ', spaceId)
+  if (spaceError) {
+    console.log(spaceError)
+    return <div>Space couldn&#39;t be found</div>
+  }
+  const { data: resources, error: resourcesError } = await supabase
     .from('resources')
     .select()
+    .eq("space_id", spaceId)
   // need to find a way to select only the resources from the respective space
-  console.log(resources)
-  if (error) {
-    console.error('Error fetching spaces:', error);
+  // console.log(resources)
+  if (!resources) {
+    return <div>No resources available for the space</div>
+    console.log(resourcesError)
   }
 
   return (
