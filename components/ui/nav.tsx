d4eaed6dff71480e930f8e1a286666c233ca9738
Malik Piara <malik@hey.com>
2024-06-20 00:37:54 +0200

    Started reworking navbar. Added our logo.

--- components/ui/nav.tsx
+++ components/ui/nav.tsx
@@ -1,35 +1,55 @@
-import Link from "next/link";
-import { Button } from '@/components/ui/button'
-import { createClient } from "@/utils/supabase/server";
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import { createClient } from '@/utils/supabase/server';
+import { Popsicle } from 'lucide-react';
+import logo from '@/logo.svg';
+import Image from 'next/image';
 
 export default async function Nav() {
-  const supabase = createClient()
-  const { data: { user }, error } = await supabase.auth.getUser()
+  const supabase = createClient();
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
 
   return (
-    <div className="w-full flex justify-between p-4">
-      <p className="p-2 start">Hello, {user ? `${user?.email}!` : 'stranger!'} </p>
-      <div className="flex space-x-4">
-        {user && (
+    <div className='w-full flex justify-between p-4'>
+      <div className='flex'>
+        <Link href={'/home'} className='hover:animate-pulse'>
+          <Image
+            className='-space-y-10'
+            src={'/logo.svg'}
+            alt='logo'
+            width={50}
+            height={50}
+          />
+        </Link>
+      </div>
 
-          <Link className='p-2 hover:underline hover:underline-offset-4' href="/hello">Welcome</Link>
+      <div className='flex space-x-4'>
+        <div className='flex'>
+          <Link
+            className='p-2 hover:underline hover:underline-offset-4'
+            href='/home'
+          >
+            Spaces
+          </Link>
+          {user && (
+            <>
+              <Link
+                className=' p-2 hover:underline hover:underline-offset-4'
+                href={`/account`}
+              >
+                Profile
+              </Link>
 
-        )}
-
-        <Link className='p-2 hover:underline hover:underline-offset-4' href="/home">Spaces</Link>
-        {user && (
-          <>
-            <Link className=" p-2 hover:underline hover:underline-offset-4" href={`/account`}>Profile</Link>
-
-            <form action="/auth/signout" method="post">
-              <Button type="submit">
-                Sign out
-              </Button>
-            </form>
-
-          </>
-        )}
+              <form action='/auth/signout' method='post'>
+                <Button type='submit'>Sign out</Button>
+              </form>
+            </>
+          )}
+        </div>
       </div>
     </div>
-  )
+  );
 }
