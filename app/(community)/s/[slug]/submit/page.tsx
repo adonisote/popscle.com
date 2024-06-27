3e6a82578ba62568b5147cf020dfe5aef3a384a6
adonisote <adonis.almagro@code.berlin>
2024-06-27 16:32:23 +0200

    submit resource page as server component

--- app/(community)/s/[slug]/submit/page.tsx
+++ app/(community)/s/[slug]/submit/page.tsx
@@ -1,50 +1,27 @@
-'use client'
+import { createClient } from '@/utils/supabase/server';
+import SpaceNotFound from './spaceNotFound';
+import { Suspense } from 'react';
+import { redirect } from 'next/navigation';
+import { ResourceForm } from './submitResourceForm';
 
-import { createClient } from "@/utils/supabase/client"
-import { ResourceForm } from "./submitResourceForm"
-import { useEffect, useState } from "react"
-import { useRouter } from "next/navigation"
-
-
-export default function SubmitPage({ params }: { params: { slug: string } }) {
+export default async function Page({ params }: { params: { slug: string } }) {
   const supabase = createClient()
-  const router = useRouter()
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState<string | null>(null)
-
-  useEffect(() => {
-    const fetchSpace = async () => {
-      const { data: spaces, error: spaceError } = await supabase
-        .from('spaces')
-        .select('id, title')
-        .ilike('title', params.slug); //all space titels have to be change to be lowercase as entries. then we can use .eq()
-      if (!spaces || spaces.length === 0) {
-        console.log('Error fetching Space:', spaceError)
-        setError('Space not found. Redirecting to home...')
-        setTimeout(() => {
-          router.push('/home')
-        }, 3000) // Wait for 2 seconds before redirecting
-      } else {
-        setLoading(false)
-      }
-    }
-    fetchSpace()
-  }, [supabase, router, params.slug])
-
-  if (error) {
-    return (
-      <div>
-        <p>{error}</p>
-      </div>
-    )
+  const { data: spaces, error: spaceError } = await supabase
+    .from('spaces')
+    .select('id, title, description, created_at')
+    .ilike('title', params.slug); //replace with .eq() when making sure that all titles are lowercae
+  // If there is no space with the id redirect to /home
+  if (!spaces || spaces.length === 0) {
+    console.log('spaceError;:', spaceError);
+    // return <SpaceNotFound />  //Use to redirect to home within 3 seconds
+    redirect('/home')
   }
-  if (loading) {
-    return (
-      <div>
-        <p>Loading...</p>
-      </div>
-    )
+  const { data: { user }, error: userError } = await supabase
+    .auth.getUser()
+  if (!user) {
+    redirect('/login')
   }
+  console.log('User id:', user?.id)
   // Function to convert a string to title case
   function toTitleCase(str: string): string {
     return str
@@ -53,11 +30,95 @@ export default function SubmitPage({ params }: { params: { slug: string } }) {
       .map(word => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalize the first letter of each word
       .join(' '); // Join the words back together with a space
   }
+  const spaceId = spaces[0]?.id;
+  const spaceTitle = spaces[0]?.title;
+  const userId = user?.id
 
   return (
     <div>
-      <p>Submit a new resource  for {toTitleCase(params.slug)}</p>
-      <ResourceForm />
+      <p className='text-center'>
+        Submit new resource for {toTitleCase(spaceTitle)}
+      </p>
+      <ResourceForm spaceId={spaceId} userId={userId} />
+
     </div>
   )
+
 }
+
+//Client Component:
+
+
+// 'use client'
+// //this could be a server component. I would need to pass the spaceid to a client component.
+// import { createClient } from "@/utils/supabase/client"
+// import { ResourceForm } from "./submitResourceForm"
+// import { useEffect, useState, useCallback } from "react"
+// import { useRouter } from "next/navigation"
+
+
+// export default function SubmitPage({ params }: { params: { slug: string } }) {
+//   const supabase = createClient()
+//   const router = useRouter()
+//   const [loading, setLoading] = useState(true)
+//   const [error, setError] = useState<string | null>(null)
+//   const [spaceId, setSpaceId] = useState<string | null>(null)
+
+//   const fetchSpace = useCallback(async () => {
+//     try {
+//       const { data: spaces, error: spaceError } = await supabase
+//         .from('spaces')
+//         .select('id, title')
+//         .ilike('title', params.slug) // Ensure titles in the database are lowercase
+
+//       if (!spaces || spaces.length === 0) {
+//         console.log('Error fetching Space:', spaceError)
+//         setError('Space not found. Redirecting to home...')
+//         setTimeout(() => {
+//           router.push('/home')
+//         }, 3000) // Wait for 3 seconds before redirecting
+//       } else {
+//         setSpaceId(spaces[0]?.id)
+//         setLoading(false)
+//       }
+//     } catch (error) {
+//       console.error('Unexpected error:', error)
+//       setError('An unexpected error occurred. Please try again later.')
+//     }
+//   }, [params.slug, router, supabase])
+
+//   useEffect(() => {
+//     fetchSpace()
+//   }, [fetchSpace])
+
+
+//   if (error) {
+//     return (
+//       <div>
+//         <p>{error}</p>
+//       </div>
+//     )
+//   }
+//   if (loading) {
+//     return (
+//       <div>
+//         <p>Loading...</p>
+//       </div>
+//     )
+//   }
+//   // Function to convert a string to title case
+//   function toTitleCase(str: string): string {
+//     return str
+//       .toLowerCase() // Convert the entire string to lowercase
+//       .split(/[\s\-]+/) // Split the string by spaces or hyphens
+//       .map(word => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalize the first letter of each word
+//       .join(' '); // Join the words back together with a space
+//   }
+
+//   return (
+//     <div>
+//       <p>Submit a new resource  for {toTitleCase(params.slug)}</p>
+//       <ResourceForm spaceId={spaceId} />
+//     </div>
+//   )
+// }
